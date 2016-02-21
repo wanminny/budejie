4ac9f084a2ef6d0001b18cd03cdc67d786161d01
wanmin <min.wan@yoho.cn>
2016-02-21 11:35:13 +0800

    Initial Commit

---
+++ 不得姐/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  不得姐
+//
+//  Created by wan on 16/2/21.
+//  Copyright © 2016年 wan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
